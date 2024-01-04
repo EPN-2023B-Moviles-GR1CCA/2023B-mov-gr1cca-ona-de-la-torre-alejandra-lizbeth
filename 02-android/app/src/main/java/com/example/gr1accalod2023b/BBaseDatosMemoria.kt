f5d546063ac2664df3c279b6bddec1f48c235a99
Alejandra Oña <alejandra.onadelatorre@epn.edu.ec>
2024-01-04 17:07:46 -0500

    feat: intends and app distribution

--- 02-android/app/src/main/java/com/example/gr1accalod2023b/BBaseDatosMemoria.kt
+++ 02-android/app/src/main/java/com/example/gr1accalod2023b/BBaseDatosMemoria.kt
@@ -1,4 +1,24 @@
 package com.example.gr1accalod2023b
 
 class BBaseDatosMemoria {
+    //companion object
+
+    companion object{
+        val arregloBEntrenador = arrayListOf<BEntrenador>()
+
+        init {
+            arregloBEntrenador
+                .add(
+                    BEntrenador(1, "Alejandra", "a@a.com")
+                )
+            arregloBEntrenador
+                .add(
+                    BEntrenador(2, "Lizbeth", "b@b.com")
+                )
+            arregloBEntrenador
+                .add(
+                    BEntrenador(3, "Carolina", "c@c.com")
+                )
+        }
+    }
 }
--- 02-android/app/src/main/java/com/example/gr1accalod2023b/BEntrenador.kt
+++ 02-android/app/src/main/java/com/example/gr1accalod2023b/BEntrenador.kt
@@ -1,4 +1,11 @@
 package com.example.gr1accalod2023b
 
-class BEntrenador {
+class BEntrenador ( //representacion del modelo
+    var id: Int,
+    var nombre: String?,
+    var descripcion: String?
+){
+    override fun toString(): String {
+        return "${nombre} - ${descripcion}"
+    }
 }
