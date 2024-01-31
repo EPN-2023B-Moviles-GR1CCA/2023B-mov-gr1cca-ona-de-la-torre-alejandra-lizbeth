faf3310e2c8b6347ad38d75d2e9830fe43e8dfb2
Alejandra Oña <alejandra.onadelatorre@epn.edu.ec>
2024-01-31 17:27:09 -0500

    feat: creacion de interfaz y entidad para uso de cloud firestore

---
+++ 02-android/app/src/main/java/com/example/gr1accalod2023b/IFirestore.kt
@@ -0,0 +1,35 @@
+package com.example.gr1accalod2023b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QueryDocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.*
+import kotlin.collections.ArrayList
+
+class IFirestore : AppCompatActivity() {
+
+    var query: Query? = null
+    val arreglo: ArrayList<ICities> = arrayListOf()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ifirestore)
+
+        //Configuracion del list view
+        val listView = findViewById<ListView>(R.id.lv_firestore)
+        val adaptador = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            arreglo
+        )
+        listView.adapter = adaptador
+        adaptador.notifyDataSetChanged()
+    }
+}
